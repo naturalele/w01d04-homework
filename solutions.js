92050a5e23478d18ddddcf6cd8abd3d5eed726bf
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-14 20:37:34 -0600

    Commit 1 - Verbal questions

---
+++ solutions.js
@@ -0,0 +1,7 @@
+// 1. Verbal questions
+
+// A parameter is part of a function and is defined prior to {}'s
+// A argument is an expression and used at the end of a function to call it back
+
+// Return is part of the function within {}'s which gives a value  
+// and console.log is a message for the computer which displays information in console
